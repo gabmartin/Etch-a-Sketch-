ee4bf87f36839d5a52b89fb69e4267e54825151f
Gabriel Martín <gabmarod@gmail.com>
2023-04-07 13:32:39 +0200

    Add gridSize functionality

--- js/script.js
+++ js/script.js
@@ -1,31 +1,46 @@
-let container = document.querySelector('section');
-let columns = 16;
-let rows = 16;
+let container = document.querySelector('#container');
+let gridSize = 16;
 
-// Creating the columns
-for (let i = 0; i < columns; i++) { 
-    
-    let column = document.createElement('div');
-    column.classList.add('column');
-    container.appendChild(column);
+function createGrid(gridSize){
+    // Creating the columns
+    for (let i = 0; i < gridSize; i++) { 
+        
+        let column = document.createElement('div');
+        column.classList.add('column');
+        container.appendChild(column);
 
-    // Creating every square
-    for (let i = 0; i < 16; i++) { 
-    
-        let unit = document.createElement('div');
-        unit.classList.add('unit');
-        column.appendChild(unit);
-    
-    } 
+        // Creating every square
+        for (let i = 0; i < gridSize; i++) { 
+            let unit = document.createElement('div');
+            unit.classList.add('unit');
+            unit.style.width = `calc(30vw/${gridSize})`;
+            unit.style.height = `calc(30vw/${gridSize})`;
+            column.appendChild(unit);
+        } 
+    }
 
+    // Hovering effect
+    let unitsArray = document.querySelectorAll('.unit');
+    unitsArray.forEach(function(unit){
+        unit.addEventListener("mouseover", function(){
+            unit.classList.add("black");
+        });
+    });
 }
 
-// Hovering effect
-let unitsArray = document.querySelectorAll('.unit');
-unitsArray.forEach(function(unit){
-    unit.addEventListener("mouseover", function(){
-        unit.classList.add("black");
-    });
-});
+createGrid(gridSize);
+
+function promptWindow(){
+    let unitsArray = document.querySelectorAll('.unit');
+    unitsArray.forEach(unit => unit.remove());
+    gridSize = prompt('Cantidad de pixeles', 16);
+    createGrid(gridSize);
+}
+
+// Reset button and popup window
+let popBtn = document.querySelector('#reset');
+popBtn.addEventListener("click", promptWindow);
+
+
 
 
